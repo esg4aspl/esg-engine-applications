68d9e91171ec9934edac9b543a32812ad3e4da2a
dilekozturk93 <ozturkdilek93@gmail.com>
2022-07-01 15:38:58 +0300

    initial commit

---
+++ src/main/java/JSONFileWithSchemaValidationApp.java
@@ -0,0 +1,19 @@
+
+
+import tr.edu.iyte.esg.conversion.json.JSONFileToModelConverterWithSchemaValidation;
+
+
+public class JSONFileWithSchemaValidationApp {
+
+	public static void main(String[] args) {
+
+		String schemaFile = "files/JSONFiles/ESGSimpleWithMultipleEdgesFromVertexSchema.json";
+		String JSONFile = "files/JSONFiles/ESGEulerianWithMultipleEdgesFromVertex.json";
+
+		JSONFileToModelConverterWithSchemaValidation jsonFileToModelConverterWithSchemaValidation = new JSONFileToModelConverterWithSchemaValidation(
+				schemaFile, JSONFile);
+
+		jsonFileToModelConverterWithSchemaValidation.JSONSchemaValidation();
+	}
+
+}
